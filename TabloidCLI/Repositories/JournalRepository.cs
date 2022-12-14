29a73b2e9de62949f4584c976c084369b5beac58
Seaborn Mercer <surfermercer@gmail.com>
2022-12-14 14:36:47 -0600

    Added journal model, menu option, and add

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    public class JournalRepository : DatabaseConnector
+    {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+        public List<Journal> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id,
+                                               Title,
+                                               Content,
+                                               CreateDateTime
+                                          FROM Journal";
+
+                    List<Journal> journals = new List<Journal>();
+                    
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                        };
+                        journals.Add(journal);
+                    }
+
+                    reader.Close();
+
+                    return journals;
+                }
+            }
+        }
+
+        public void Insert(Journal journal)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Journal (Title, Content, CreateDateTime )
+                                                     VALUES (@Title, @Content, @CreateDateTime)";
+                    cmd.Parameters.AddWithValue("@Title", journal.Title);
+                    cmd.Parameters.AddWithValue("@Content", journal.Content);
+                    cmd.Parameters.AddWithValue("@CreateDateTime", journal.CreateDateTime);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        
+    }
+}
---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) Add Journal");
+            Console.WriteLine(" 2) List All Journals");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "2":
+                    GetAll();
+                    return this;
+                case "1":
+                    Add();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+       
+
+        private void Add()
+        {
+            Console.WriteLine("New Author");
+            Journal journal = new Journal();
+
+            Console.Write("Title: ");
+            journal.Title = Console.ReadLine();
+
+            Console.Write("Journal Content: ");
+            journal.Content = Console.ReadLine();
+
+            journal.CreateDateTime = DateTime.Now;
+
+            _journalRepository.Insert(journal);
+        }
+
+        private void GetAll()
+        {
+            List<Journal> journals = _journalRepository.GetAll();
+            foreach(Journal j in journals)
+            {
+                Console.WriteLine($"{j.Title}");
+            }
+            Console.Write("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
